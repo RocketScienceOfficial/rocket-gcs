79fe8ab67d29d8895bc1de7ced7bf7f61a99d0db
Filip Gawlik <filippo.gawlik@gmail.com>
2024-06-21 07:45:01 +0200

    Added BT switch

--- src/measurements.cpp
+++ src/measurements.cpp
@@ -1,32 +1,42 @@
 #include "measurements.h"
 #include "config.h"
 #include <Arduino.h>
+
+#ifdef BT_NAME
 #include <BluetoothSerial.h>
 
 static BluetoothSerial SerialBT;
+
+#define LOGGER SerialBT
+#else
+#define LOGGER Serial
+#endif
+
 static MeasurementData s_CurrentMeasurement;
 
 void MeasInit()
 {
+#ifdef BT_NAME
     SerialBT.begin(BT_NAME);
+#endif
 }
 
 void SetMeasurementData(MeasurementData *data)
 {
-    SerialBT.print("/*");
-    SerialBT.printf("%.1f,", data->roll);
-    SerialBT.printf("%.1f,", data->pitch);
-    SerialBT.printf("%.1f,", data->yaw);
-    SerialBT.printf("%.1f,", data->velocity);
-    SerialBT.printf("%.1f,", data->batteryVoltage);
-    SerialBT.printf("%d,", data->batteryPercentage);
-    SerialBT.printf("%.7f,", data->latitude);
-    SerialBT.printf("%.7f,", data->longitude);
-    SerialBT.printf("%d,", data->altitude);
-    SerialBT.printf("%d,", data->state);
-    SerialBT.printf("%d,", data->signalStrength);
-    SerialBT.printf("%d,", data->packetLoss);
-    SerialBT.println("*/");
+    LOGGER.print("/*");
+    LOGGER.printf("%.1f,", data->roll);
+    LOGGER.printf("%.1f,", data->pitch);
+    LOGGER.printf("%.1f,", data->yaw);
+    LOGGER.printf("%.1f,", data->velocity);
+    LOGGER.printf("%.1f,", data->batteryVoltage);
+    LOGGER.printf("%d,", data->batteryPercentage);
+    LOGGER.printf("%.7f,", data->latitude);
+    LOGGER.printf("%.7f,", data->longitude);
+    LOGGER.printf("%d,", data->altitude);
+    LOGGER.printf("%d,", data->state);
+    LOGGER.printf("%d,", data->signalStrength);
+    LOGGER.printf("%d,", data->packetLoss);
+    LOGGER.println("*/");
 
     s_CurrentMeasurement = *data;
 }
